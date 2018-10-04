b3393106a04367d37a4c6a827c58dcf907f8bc4a
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2018-10-04 09:46:12 +0200

    Test

---
+++ 05 - Eenvoudige Functies/Test.py
@@ -0,0 +1,17 @@
+from math import sqrt, pi
+
+getal = float(input('getal: '))
+
+print(sqrt(getal - pi))
+
+from random import random, randint, seed
+
+print(randint(10,15))
+print(randint(10,15))
+
+print(random())
+print(random())
+
+seed(1267)
+
+# random is niet echt random
