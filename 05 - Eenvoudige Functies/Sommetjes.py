2d8ecb9ee9ff834f2f3a6873c0a9bac8cbef7a52
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2018-10-07 18:23:29 +0200

    sommetjes

--- 05 - Eenvoudige Functies/Sommetjes.py
+++ 05 - Eenvoudige Functies/Sommetjes.py
@@ -2,13 +2,23 @@
 a = float(input('Getal a is '))
 b = float(input('Getal b is '))
 
+i = 0
+berekening = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((10 ** i) * a, (10 ** i) * b, (10 ** i) * a  + (10 ** i) * b)
+
 i = 1
+berekening_2 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((10 ** i) * a, (10 ** i) * b, (10 ** i) * a  + (10 ** i) * b)
 
-#berekening
-linkerlid_1 = (10 ** i) * a
-linkerlid_2 = (10 ** i) * b
-rechterlid = (10 ** i) * a  + (10 ** i) * b
+i = 2
+berekening_3 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((10 ** i) * a, (10 ** i) * b, (10 ** i) * a  + (10 ** i) * b)
 
+i = 3
+berekening_4 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((10 ** i) * a, (10 ** i) * b, (10 ** i) * a  + (10 ** i) * b)
 
-berekening = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format(linkerlid_1, linkerlid_2, rechterlid)
+i = 4
+berekening_5 = '{:>6.0f} + {:<6.0f} = {:<6.0f}'.format((10 ** i) * a, (10 ** i) * b, (10 ** i) * a  + (10 ** i) * b)
 
+print(berekening)
+print(berekening_2)
+print(berekening_3)
+print(berekening_4)
+print(berekening_5)
