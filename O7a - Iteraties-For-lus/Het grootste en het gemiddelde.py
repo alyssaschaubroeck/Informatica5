184aab63e91b7af2f34456b0d396de7cb374dc85
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2018-11-12 19:51:27 +0100

    rij van fibonacci

--- O7a - Iteraties-For-lus/Het grootste en het gemiddelde.py
+++ O7a - Iteraties-For-lus/Het grootste en het gemiddelde.py
@@ -1,5 +1,18 @@
-getallen = input(('getallen: '))
+#input
+aantal = int(input('aantal getallen: '))
+max = 0
+som = 0
 
-print(max(getallen))
+#input getallen
+for i in range(aantal):
+    getal = int(input('getal: '))
+    if i == 0:
+        max = getal
+    elif getal > max:
+        max = getal
+    som += getal
 
-#print('Het grootste getal is {} en het gemiddelde is {.2f}. format( , )')
+#bewerking
+gemiddelde = som / aantal
+
+print('Het grootste getal is {:d} en het gemiddelde is {:.2f}'. format(max, gemiddelde))
