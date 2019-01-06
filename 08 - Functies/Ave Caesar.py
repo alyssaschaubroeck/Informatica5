cb0550d8d81f2596857efd3d0ec5d4328762111c
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2019-01-06 11:31:06 +0100

    Ave Caesar

--- 08 - Functies/Ave Caesar.py
+++ 08 - Functies/Ave Caesar.py
@@ -1,23 +1,36 @@
 #input
+volledig = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 alfabet = 'abcdefghijklmnopqrstuvwxyz'
 h_alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 #letter of niet?
 def is_letter(n):
-    if n in alfabet or h_alfabet:
+    if n in volledig:
         letter = True
     else:
         letter = False
     return letter
 
 #roteer letter
-def roteer_letter(a, b):
-    if letter in woord
-    if is_letter(a) == True:
-        rotatie = chr(ord(a) + b)
-        return rotatie
+def roteer_letter(letter, cijfer):
+    if letter in alfabet:
+        if ord(letter) + cijfer > 122:
+            rotatie = chr(ord(letter) + cijfer - 26)
+        else:
+            rotatie = chr(ord(letter) + cijfer)
+    elif letter in h_alfabet:
+        if ord(letter) + cijfer > 90:
+            rotatie = chr(ord(letter) + cijfer - 26)
+        else:
+            rotatie = chr(ord(letter) + cijfer)
+    return rotatie
 
 #versleutel
-def versleutel(tekst, ceasarcijfer):
-    antwoord = roteer_letter(tekst,ceasarcijfer)
+def versleutel(tekst, cijfer):
+    antwoord = ''
+    for letter in tekst:
+        if is_letter(letter) == True:
+            antwoord += roteer_letter(letter, cijfer)
+        if is_letter(letter) == False:
+            antwoord += letter
     return antwoord
--- 08 - Functies/Vermoeden can Collatz.py
+++ 08 - Functies/Vermoeden can Collatz.py
@@ -1,15 +1,19 @@
+#volgend collatz getal
 def volgend_collatz_getal(n):
     if n % 2 == 0:
         volgend = n // 2
-    else:
-        volgend = (n * 3) + 1
+    else:        volgend = (n * 3) + 1
     return volgend
 
+#cyclelengte
 def collatz(n):
-    if volgend_collatz_getal(n) != 1:
-
-
-
+    lengte = 1
+    while volgend_collatz_getal(n) != 1:
+        lengte += 1
+        n = volgend_collatz_getal(n)
+    if volgend_collatz_getal(n) == 1:
+        lengte += 1
+    return lengte
 
 #bij dodana print niet meer ingeven
 #geen input meer en geen print meer in pycharm
