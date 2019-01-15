119f4320492a527e4888520c9e7068d6385fc760
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2019-01-15 07:14:18 +0100

    Lingo 2

--- 10 - Strings/Lingo 2.py
+++ 10 - Strings/Lingo 2.py
@@ -1,14 +1,13 @@
 def hint(gok, woord):
-    antw = '.....'
+    antw = gok
     for i in range(0,5):
         if gok[i] not in woord:
-            antw = '.....'
+            antw = antw.replace(gok[i], '.')
         elif gok[i] in woord:
             if gok.find('[i]') == woord.find('[i]'):
-                antw = antw.replace(gok[i], gok.upper())
+                antw = antw.replace(gok[i], gok[i].upper())
             else:
-                antw = gok[i]
-
+                antw = antw.replace(gok[i], gok[i].lower())
     return antw
 
 print(hint('spoed', 'depri'))
