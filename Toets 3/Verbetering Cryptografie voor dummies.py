c81027d929306c865c6ba0ae783fff6ed6f0eff1
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2019-01-24 10:03:27 +0100

    toets 3

---
+++ Toets 3/Verbetering Cryptografie voor dummies.py
@@ -0,0 +1,16 @@
+def versleutel_woord(woord, n):
+
+    versleuteld_woord = ''
+
+    woord = woord.upper()
+
+    for letter in woord:
+
+        versleutelde_letter += chr(ord(letter) + n)
+
+        if versleutelde_letter == '@':
+            versleutelde_letter = ' '
+
+            versleuteld_woord += versleutelde_letter
+
+    return versleutel_woord()
