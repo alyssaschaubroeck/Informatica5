4abab137996e8016170e30e3d223c052dcea8fcf
alyssaschaubroeck <43027991+alyssaschaubroeck@users.noreply.github.com>
2019-02-07 10:04:28 +0100

    Lists

--- 11 - Tuples/Schaakverslag.py
+++ 11 - Tuples/Schaakverslag.py
@@ -1,7 +1,8 @@
 def geldige_zet(schaakzet):
-    if schaakzet[0] in 'KDTLP' and schaakzet[1] in 'abcdefgh' and schaakzet[2] in '123456789':
+    # antw = false dan else niet nodig
+    if len(schaakzet) == 3 and schaakzet[0] in 'KDTLP' and schaakzet[1] in 'abcdefgh' and schaakzet[2] in '123456789':
         antw = True
-    elif schaakzet [0] in 'abcdefgh' and schaakzet[1] in '123456789':
+    elif len(schaakzet) == 2 and schaakzet [0] in 'abcdefgh' and schaakzet[1] in '123456789':
         antw = True
     else:
         antw = False
@@ -10,6 +11,9 @@ def geldige_zet(schaakzet):
 def geldige_zetten(schaakzetten):
     antw = True
     i = 0
+    #while i < len(schaakzetten) and geldige_zet(schaakzetten[i])
+        #i += 1
+    #return i >= len(schaakzetten)
     while antw == True and i in range(0, len(schaakzetten) - 1):
         nieuw = schaakzetten[i]
         antw = geldige_zet(nieuw)
---
+++ 12a - Lists/Dubbels.py
@@ -0,0 +1,14 @@
+def dubbels(lijst):
+
+    dubbel = []
+
+    for item in lijst:
+
+        if lijst.count([item]) > 1 and item not in dubbel:
+
+            dubbel.append(item)
+            lijst = lijst.pop(item)
+
+    return dubbel
+
+print(dubbels([(0, 1), 'joris', 4, 'korneel', (1, -1), 1, 1, 'piet', 4, 'joris']))
